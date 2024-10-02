ddf66b14bbdc9ab483747e5f7f9c99f57f7d73e3
zxllo12 <poapog123@naver.com>
2024-10-02 12:42:15 +0900

    카메라 움직임

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform trackedObject;
+    public float height = 6.5f;
+    public float undergroundHeight = -9.5f;
+    public float undergroundThreshold = 0f;
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = Mathf.Max(cameraPosition.x, trackedObject.position.x);
+        transform.position = cameraPosition;
+    }
+
+    public void SetUnderground(bool underground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = underground ? undergroundHeight : height;
+        transform.position = cameraPosition;
+    }
+}
